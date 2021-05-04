2bd812f43afb1f2f0a8c252391a0b72ab8a26b4e
montgomeryrd <montgomery_rd@yahoo.com>
2021-05-03 21:45:21 -0700

    implemented socket.io

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,6 +1,7 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import 'quill/dist/quill.snow.css';
 import Quill from 'quill';
+import io from 'socket.io-client';
 
 const TOOLBAR_OPTIONS = [
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
@@ -18,12 +19,54 @@ const TOOLBAR_OPTIONS = [
 ];
 
 const TextEditor = () => {
+    const [socket, setSocket] = useState();
+    const [quill, setQuill] = useState();
+
+    useEffect(() => {
+        const s = io('http://localhost:3001');
+        setSocket(s);
+
+        return () => {
+            s.disconnect();
+        }
+    }, []);
+
+    useEffect(() => {
+        if (socket == null || quill == null) return;
+
+        const handler = (delta) => {
+            quill.updateContents(delta);
+        };
+
+        socket.on('receive-changes', handler);
+
+        return () => {
+            socket.off('receive-changes', handler);
+        };
+    }, [socket, quill]);
+
+    useEffect(() => {
+        if (socket == null || quill == null) return;
+
+        const handler = (delta, oldDelta, source) => {
+            if (source !== 'user') return;
+            socket.emit('send-changes', delta);
+        };
+
+        quill.on('text-change', handler);
+
+        return () => {
+            quill.off('text-change', handler)
+        };
+    }, [socket, quill]);
+
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return;
         wrapper.innerHTML = '';
         const editor = document.createElement('div');
         wrapper.append(editor);
-        new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+        const q = new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+        setQuill(q);
     }, []);
 
     return (
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const io = require('socket.io')(3001, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', socket => {
+    socket.on('send-changes', delta => {
+        socket.broadcast.emit('receive-changes', delta);
+    });
+
+    console.log('%c Connected Socket 3001', 'color: yellow;');
+});
