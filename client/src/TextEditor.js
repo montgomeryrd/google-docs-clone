74a6dd3852426b9d66d5f18570916f70cccb49b0
montgomeryrd <montgomery_rd@yahoo.com>
2021-05-05 21:51:18 -0700

    installed mongoose

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,8 +1,10 @@
 import { useCallback, useEffect, useState } from 'react';
-import 'quill/dist/quill.snow.css';
+import { useParams } from 'react-router-dom';
 import Quill from 'quill';
 import io from 'socket.io-client';
+import 'quill/dist/quill.snow.css';
 
+const SAVE_INTERVAL_MS = 2000;
 const TOOLBAR_OPTIONS = [
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
     ['bold', 'italic', 'underline', 'strike'],
@@ -19,6 +21,7 @@ const TOOLBAR_OPTIONS = [
 ];
 
 const TextEditor = () => {
+    const {id: documentId} = useParams();
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
 
@@ -30,6 +33,29 @@ const TextEditor = () => {
             s.disconnect();
         }
     }, []);
+
+    useEffect(() => {
+        if (socket == null || quill == null) return;
+
+        socket.once('load-document', document => {
+            quill.setContents(document);
+            quill.enable();
+        });
+
+        socket.emit('get-document', documentId);
+    }, [socket, quill, documentId]);    
+
+    useEffect(() => {
+        if (socket == null || quill == null) return;
+
+        const interval = setInterval(() => {
+            socket.emit('save-document', quill.getContents());
+        }, SAVE_INTERVAL_MS);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [socket, quill]);
 
     useEffect(() => {
         if (socket == null || quill == null) return;
@@ -62,10 +88,18 @@ const TextEditor = () => {
 
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return;
+
         wrapper.innerHTML = '';
         const editor = document.createElement('div');
         wrapper.append(editor);
-        const q = new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+        const q = new Quill(editor, { 
+            theme: 'snow', 
+            modules: { 
+                toolbar: TOOLBAR_OPTIONS 
+            }
+        });
+        q.disable();
+        q.setText(`Loading...`);
         setQuill(q);
     }, []);
 
